0e948df00ae7636b878657aad25867a418cc2bb0
umeow0716 <jj30462281@gmail.com>
2026-07-23 18:11:48 +0800

    fix imu type

--- modules/g7_openarm_hardware/src/g7_openarm_hardware/imu_cli.py
+++ modules/g7_openarm_hardware/src/g7_openarm_hardware/imu_cli.py
@@ -13,7 +13,7 @@ from unitree_sdk2py.core.channel import (
 from unitree_sdk2py.idl.default import (
     unitree_go_msg_dds__IMUState_ as IMUState_default,
 )
-from unitree_sdk2py.idl.unitree_go.msg.dds_ import IMUState_
+from unitree_sdk2py.idl.unitree_hg.msg.dds_ import IMUState_
 
 from .config import config
 
